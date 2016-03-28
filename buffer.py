3faa6cc545d93c91b20e6da376a5362b37842b85
Wenjun.Zhang <big.zhang@outlook.com>
2016-03-29 07:33:51 +0800

    Add threads.

--- buffer.py
+++ buffer.py
@@ -1,5 +1,6 @@
 import parser
 import struct
+import threading
 
 class Buffer(object):
     """Receive data for phone and then
@@ -14,17 +15,33 @@ class Buffer(object):
     mData = None
     mSize = 0
     mPos = 0
+    mRecv_Thread = None
+    mProc_Thread = None
 
     cur_head = None
     cur_buff_type = -1
     cur_buff_size = 0
     cur_buff = None
+    mutex = None
 
     def __init__(self, con, size=4 *1024 * 1024):
         """Inits SampleClass with blah."""
         self.mCon = con
         self.mSize = size
         self.mPar = parser.Parser()
+        self.mutex = threading.Lock()
+        self.mRecv_Thread = threading.Thread(target=self.receive_main, args=(10,))
+        self.mProc_Thread = threading.Thread(target=self.process_main, args=(10,))
+
+    def receive_main(self):
+        self.mutex.acquire()
+        #TODO: Receive Data
+        self.mutex.release()
+
+    def process_main(self):
+        self.mutex.acquire()
+        # TODO: Parse and collection
+        self.mutex.release()
 
     def process_head(self):
         """Receive data to buff."""
