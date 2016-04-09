80760d7e0b32f229011035257804537ce501f277
Wenjun.Zhang <zhangwenjun1@meizu.com>
2016-04-09 18:09:10 +0800

    Add setup.py and fixed adb root can not connect issue.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyGator',
+    version='v1',
+    packages=[''],
+    url='',
+    license='',
+    author='Wenjun.Zhang',
+    author_email='zhangwenjun1@meizu.com',
+    description=''
+)
--- setup_adb.sh
+++ setup_adb.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 adb root
+sleep 5
 adb forward tcp:8084 tcp:8080
-
 adb shell gatord
